eb560d1aa9a08190f6fae4c4fd9dc84d3e1b191e
Andreas Kruhlmann <andreas@kruhlmann.com>
2019-02-15 14:28:53 +0100

    fixed linting errors

--- src/parser.js
+++ src/parser.js
@@ -8,6 +8,7 @@ const request = require("request-promise");
 const discord = require("discord.js");
 const path = require("path");
 const fs = require("fs");
+const lib = require("./lib");
 const Nightmare = require("nightmare");
 const plugin = require("nightmare-screenshot");
 
@@ -24,10 +25,10 @@ const large_icon_stub = "https://classicdb.ch/images/icons/large";
 const tooltip_stub = "http://kruhlmann-code.com:8080/classicdb_bot/item_cache";
 
 /**
- * Takes a screenshot of an item tooltip on the classicdb website, and saves 
+ * Takes a screenshot of an item tooltip on the classicdb website, and saves
  * it to the disk. The image is saved as `img/${item_id}.png`. Function is sync
  * so the image can be accessed immediately after running.
- * 
+ *
  * @param {number} item_id - ID of the item to build an image from.
  */
 function build_item_image(item_id) {
@@ -38,14 +39,14 @@ function build_item_image(item_id) {
         // Setting a large viewport size ensures the entire tooltip can be seen.
         .viewport(2000, 2000)
         .goto(`https://classicdb.ch/?item=${item_id}`)
-        .use(plugin.screenshotSelector(output_path, html_selector, on_error))
-        .run(() => console.log(`Exported ${output_path}`));
+        .use(plugin.screenshotSelector(output_path, html_selector, lib.on_error))
+        .run();
 }
 
 /**
  * Finds the first item in the list with item type 3 (Item)
- * 
- * @param {Array<Array<number>>} item_details - List of item details; id, 
+ *
+ * @param {Array<Array<number>>} item_details - List of item details; id,
  * quality, item type and thumbnail.
  * @returns {number} - Index of the item if found, else -1.
  */
@@ -58,8 +59,8 @@ function find_first_item_index(item_details) {
 
 /**
  * Builds a discord RichEmbed message object from an item.
- * 
- * @param {{id: number, name: string, img: string, quality: number}} item - 
+ *
+ * @param {{id: number, name: string, img: string, quality: number}} item -
  * Item to build message around.
  * @param {string} description - Optional description for the message object.
  * @returns {discord.RichEmbed} - Rich message object.
@@ -91,7 +92,7 @@ function build_rich_message(item, description) {
 
 /**
  * Test whether a string is a representation of a numerical integet.
- * 
+ *
  * @param {string} str - String to test.
  * @returns {boolean} - True if string represents a numerical integer.
  */
@@ -128,7 +129,7 @@ function build_message_from_id(id) {
 
 /**
  * Builds a rich message response if the user requested an item.
- * 
+ *
  * @param {string} message_content - Content of the message recieved.
  * @returns {Promise<discord.RichEmbed|undefined>} - Rich message object if no
  * error occurred else undefined.
@@ -137,15 +138,15 @@ function get_message_response(message_content) {
     let match = get_item_request(message_content);
     if (!match) return;
     // If the match is an id build the message with that in mind.
-    return is_string_numerical_int(match) 
+    return is_string_numerical_int(match)
         ? build_message_from_id(match)
         : build_message_from_query(match);
 }
 
 /**
  * Finds potential search matches for [item_name].
- * 
- * @param {string} msg_content 
+ *
+ * @param {string} msg_content - Content of the message to parse.
  * @returns {string|undefined} - First match if any were found else undefined.
  */
 function get_item_request(msg_content) {
