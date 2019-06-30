faa4f2650055793d920b6adb5da01bda6b937ec3
Andreas Kruhlmann <andreas@kruhlmann.com>
2019-06-30 22:49:53 +0200

    Added crafting sources to itemization parser

--- src/parsers/itemization/parser.ts
+++ src/parsers/itemization/parser.ts
@@ -15,8 +15,21 @@ import { ItemizationItem,
          ItemizationItemMeta,
          ItemizationQuery,
          Parser} from "../../typings/types.js";
+import { build_messages_i } from "../classicdb/parser.js";
 
 const search_url_stub = "https://itemization.info/?search=";
+const proffesions: {[s: string]: string} = {
+    Alchemy: "Alchemists",
+    Blacksmithing: "Blacksmiths",
+    Cooking: "Cooks",
+    Enchanting: "Enchanters",
+    Engineering: "Engineers",
+    Fishing: "Fishermen",
+    Herbalism: "Herbalists",
+    Leatherworking: "Leatherworkers",
+    Mining: "Mining",
+    Tailoring: "Tailors",
+};
 
 function make_damage_tooltip(item: ItemizationItem): string {
     let result = "";
@@ -62,9 +75,11 @@ function make_slot_tooltip(item: ItemizationItem): string {
         }
         return `**${prefix} ${suffix}**`;
     } else {
-        return item.Slot === "Shield" || item.Slot === "Back"
-            ? item.Slot
-            : `${item.Subtype} ${item.Slot}`;
+        if (item.Slot === "Shield" || item.Slot === "Back") {
+            return item.Slot;
+        } else {
+            return `${item.Subtype} ${item.Slot}`;
+        }
     }
 }
 
@@ -90,8 +105,15 @@ function make_source_tooltip(item: ItemizationItem): string {
         // Entity in this case represents the name of a quest.
         return `Source: Awarded from _${item.Source.Entity}_ ${zone}\n`;
     }
+
     if (item.Source.Type === "Drop" && item.Source.Entity) {
         return `Source: Dropped by ${entity} ${zone}\n`;
+    }
+
+    if (item.Source.Type === "Crafting" && item.Source.Entity) {
+        const profession = proffesions[item.Source.Entity]
+            || item.Source.Entity;
+        return `Source: Crafted by [${profession}](${eurl}) ${zone}\n`;
     }
 
     return "";
@@ -117,7 +139,8 @@ function make_patch_tooltip(item: ItemizationItem,
 
 function item_to_message_desc(im: ItemizationItemMeta, patch?: string): string {
     const item_liststing = [im.Current, ...im.Previous];
-    const item = item_liststing.find((i) => i.Patch === patch) || im.Current;
+    const found_patched_item = item_liststing.find((i) => i.Patch === patch);
+    const item = found_patched_item || im.Current;
 
     return `${item.Bonding ? `${item.Bonding}\n` : ""}`
             + `${item.Unique ? "Unique\n" :  ""}`
@@ -159,44 +182,68 @@ export function get_item_request(message_content: string): ItemizationQuery {
     return {item: null, patch: null};
 }
 
+function fetch_items(query: ItemizationQuery) {
+    return request({
+        body: {
+            apiKey: config.itemization_token,
+            query: query.item,
+        },
+        json: true,
+        method: "POST",
+        uri: "https://itemization.info/api/search",
+    });
+}
+
+function build_discord_message(query: ItemizationQuery,
+                               item: ItemizationItemMeta,
+                               ): RichEmbed[] {
+    const computed_color = quality_colors_itemization[item.Current.Quality];
+    return [new RichEmbed()
+        .setColor(computed_color || "#fff")
+        .setTitle(`${item.Current.Name} ${query.patch
+            ? `(patch ${query.patch})`
+            : "(latest patch)"}`)
+        .setDescription(item_to_message_desc(item, query.patch))
+        .setAuthor("Classic DB Bot (itemization.info)",
+                   favicon_path,
+                   discord_href)
+        .setThumbnail(`https://itemization.info/icons/${item.Icon}.png`)
+        .setFooter("https://discord.gg/mRUEPnp", discord_icon)
+        .setURL(`https://itemization.info/item/${item.ID}`)];
+}
+
 export class ItemizationParser implements Parser {
+
     public async respond_to(message: string) {
         const query = get_item_request(message);
         if (!query.item || query.item === "") {
             return;
         }
-        const items = await request({
-            body: {
-                apiKey: config.itemization_token,
-                query: query.item,
-            },
-            json: true,
-            method: "POST",
-            uri: "https://itemization.info/api/search",
-        });
-
+        const items = await fetch_items(query);
         const item: ItemizationItemMeta = items[0] || null;
+
         if (!item) {
-            return [new RichEmbed()
-                .setDescription(`Unable to find item "${query.item}"`)];
+            const err_message = `Unable to find item "${query.item}"`;
+            return [new RichEmbed().setDescription(err_message)];
         }
 
         if (!item.Previous) {
             item.Previous = [{
-                Unique: false,
                 ItemLevel: 0,
-                Patch: "999999999999",
                 Name: "Unknown",
+                Patch: "999999999999",
                 Quality: "",
                 Slot: "",
-                Type: "",
                 Subtype: "",
+                Type: "",
+                Unique: false,
             }];
         }
 
         const item_had_patch_data = !item.Previous.find((i) => {
             return i.Patch === query.patch;
         }) && item.Current.Patch !== query.patch;
+
         if (query.patch && item_had_patch_data) {
             let suffix = `**[${item.Current.Name}]**\n`;
             [item.Current, ...item.Previous].forEach((i) => {
@@ -208,18 +255,6 @@ export class ItemizationParser implements Parser {
                 + `patch entries:\n\n${suffix}`)];
         }
 
-        const computed_color = quality_colors_itemization[item.Current.Quality];
-        return [new RichEmbed()
-            .setColor(computed_color || "#fff")
-            .setTitle(`${item.Current.Name} ${query.patch
-                ? `(patch ${query.patch})`
-                : "(latest patch)"}`)
-            .setDescription(item_to_message_desc(item, query.patch))
-            .setAuthor("Classic DB Bot (itemization.info)",
-                       favicon_path,
-                       discord_href)
-            .setThumbnail(`https://itemization.info/icons/${item.Icon}.png`)
-            .setFooter("https://discord.gg/mRUEPnp", discord_icon)
-            .setURL(`https://itemization.info/item/${item.ID}`)];
+        return build_discord_message(query, item);
     }
 }
