29bf2fb8e87b9d4289ca7b839e9c9a3d9f677514
Andreas Kruhlmann <andreas@kruhlmann.com>
2019-07-24 00:44:05 +0200

    Refactored classicdb effects parser

--- src/parsers/classicdb/effect.ts
+++ src/parsers/classicdb/effect.ts
@@ -6,7 +6,9 @@
 
 import * as cheerio from "cheerio";
 import { RichEmbed } from "discord.js";
+import { stat } from "fs";
 import * as request from "request-promise";
+
 import * as config from "../../../config.json";
 import { html_tag_regex, misc_icon } from "../../consts.js";
 import { fetch_thumbnail } from "../../lib.js";
@@ -42,6 +44,15 @@ export class Effect {
     public static async from_effect_html(id: string,
                                          html: string,
                                          trigger: string): Promise<Effect> {
+        // Function for finding range and cast speed for an effect.
+        const range_and_cast_format = (r1: RegExp,
+                                       r2: RegExp,
+                                       lines: string[]): string[] =>
+            (lines.find((line) => {
+                const match = (line || "").match(r1) || (line || "").match(r2);
+                return (match || []).length > 0;
+            }) || "").split(" ");
+
         const $ = cheerio.load(html);
         const tables = $("div.tooltip > table tbody tr td").children("table");
 
@@ -54,27 +65,20 @@ export class Effect {
         const stats_td = $(stat_table).find("td").first();
         const stat_html_lines = stats_td.html().split(html_tag_regex);
 
-        const range_line = (stat_html_lines.find((line) => {
-            const r1 = /[0-9]+ yd range/g;
-            const r2 = /Melee Range/g;
-            const match = (line || "").match(r1) || (line || "").match(r2);
-            return (match || []).length > 0;
-        }) || "").split(" ");
-        const cast_time_line = (stat_html_lines.find((line) => {
-            const r1 = /[0-9]+ sec cast/g;
-            const r2 = /Instant/g;
-            const match = (line || "").match(r1) || (line || "").match(r2);
-            return (match || []).length > 0;
-        }) || "").split(" ");
+        const range_line = range_and_cast_format(/[0-9]+ yd range/,
+                                                 /Melee Range/g,
+                                                 stat_html_lines);
+
+        const cast_time_line = range_and_cast_format(/[0-9]+ sec cast/g,
+                                                     /Instant/g,
+                                                     stat_html_lines);
 
         const name = stats_td.find("a").first().text();
         const thumbnail = await fetch_thumbnail(id, true);
         const range = range_line.length > 1
-            ? range_line === ["Melee", "Range"] ? "Melee Range" : range_line[0]
+            ? range_line.includes("Melee") ? "Melee Range" : range_line[0]
             : null;
-        const cast_time = cast_time_line.length > 1
-            ? range_line === ["Instant"] ? "Instant" : cast_time_line[0]
-            : null;
+        const cast_time = cast_time_line[0] || null;
         const description = $(misc_table).find("span.q").first().text() || "";
         const is_misc = thumbnail === misc_icon;
         return new Effect(id, name, `${config.host}/?spell=${id}`, description, thumbnail, trigger, is_misc, cast_time, range);
@@ -107,7 +111,6 @@ export class Effect {
     /**
      * Constructor
      *
-     * @constructor
      * @param id - Database effect id.
      * @param name - Effect name.
      * @param href - Database website link.
@@ -136,7 +139,7 @@ export class Effect {
         this.name = name;
         this.href = href;
         this.description = description;
-        this.thumbnail_href = thumbnail_href,
+        this.thumbnail_href = thumbnail_href;
         this.trigger_name = trigger_name;
         this.is_misc = is_misc;
         this.cast_time = cast_time;
@@ -169,7 +172,7 @@ export class Effect {
                 : `**${this.cast_time} second cast**\n`
             : "";
         const range = this.range
-            ? this.range === "Melee"
+            ? this.range === "Melee Range"
                 ? `**${this.range}**\n`
                 : `**${this.range} yards range**\n`
             : "";
