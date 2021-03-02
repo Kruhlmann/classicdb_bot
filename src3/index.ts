9853a2f9f9c4b6fc0916568eff3aa79c152b1b0b
Andreas Krühlmann <andreas@kruhlmann.dev>
2021-03-03 00:39:42 +0100

    :construction: Empty main

--- src3/index.ts
+++ src3/index.ts
@@ -3,9 +3,7 @@ import { PostgresDatabaseConnection } from "./database";
 import { ClassicDBBot } from "./bot";
 
 async function main(): Promise<void> {
-    //PostgresDatabaseConnection.initialize("postgres", "postgres", "classicdb_bot");
-    //ModelInitializer.initialize(PostgresDatabaseConnection.instance.database, true);
-    //await new ClassicDBBot(".XMjfcw.ammtWVSB3_q5AhaPWWn55_7MPVk").start();
+    console.log(1);
 }
 
 main();
