17ff14b905dd1458d5ef6da668bff33d499e2d3d
Andreas Krühlmann <andreas@kruhlmann.dev>
2021-03-03 01:49:36 +0100

    :white_check_mark: Added test resources

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
     testRegex: "test/.*(feature|test|spec).tsx?$",
     moduleFileExtensions: ["ts", "tsx", "js", "mjs", "json"],
     moduleNameMapper: {
-        "^src3/(.*)": "<rootDir>/src3/$1",
+        "^src/(.*)": "<rootDir>/src/$1",
     },
     moduleDirectories: ["node_modules", "src/node_modules"],
     testEnvironment: "node",
