624a9ffbf5b925648c11c03986cfb1e8c78aa325
Andreas Krühlmann <andreas@kruhlmann.dev>
2021-03-15 14:08:31 +0100

    :recycle: Refactored CSS lookup table

--- src/parsers/quality.ts
+++ src/parsers/quality.ts
@@ -1,9 +1,10 @@
 import * as cheerio from "cheerio";
 
 import { HTMLParser } from ".";
+import { LookupTable } from "../lookup_table";
 
-export class QualityParser extends HTMLParser<string> {
-    public static css_class_quality_lookup_table: Record<string, string> = {
+class CSSClassQualityLookupTable extends LookupTable<string> {
+    protected readonly lookup_table = {
         q0: "poor",
         q1: "common",
         q2: "uncommon",
@@ -13,7 +14,10 @@ export class QualityParser extends HTMLParser<string> {
         q6: "artifact",
         q7: "blizzard",
     };
+    protected readonly default_value: "poor";
+}
 
+export class QualityParser extends HTMLParser<string> {
     public parse(): string {
         const $ = cheerio.load(this.page_html_source);
         const name_node = $("div.tooltip table tr td table tr td b").first();
@@ -22,6 +26,6 @@ export class QualityParser extends HTMLParser<string> {
 
     private name_node_to_quality_string(name_node: cheerio.Cheerio): string {
         const name_node_css_class = name_node.attr("class");
-        return QualityParser.css_class_quality_lookup_table[name_node_css_class] || "";
+        return new CSSClassQualityLookupTable().perform_lookup(name_node_css_class) || "";
     }
 }
