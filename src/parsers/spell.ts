85d400aec3b74512cd428beb4cffbf4b978111bb
Andreas Krühlmann <andreas@kruhlmann.dev>
2021-03-17 02:26:42 +0100

    :white_check_mark: Added spell parser tests

--- src/parsers/spell.ts
+++ src/parsers/spell.ts
@@ -6,12 +6,41 @@ export interface PreResolvedSpellData {
     description: string;
 }
 
-export class SpellParser extends MultiRegexHTMLEffectTooltipBodyParser<PreResolvedSpellData> {
-    protected readonly pattern = /<span(?: .*?=".*?")?>(.*?)<a href="\?spell=(\d+)"(?: .*?=".*?")?>(.*?)<\/a><\/span>/g;
-    protected readonly default_value = "";
+abstract class SpellParser extends MultiRegexHTMLEffectTooltipBodyParser<PreResolvedSpellData> {
+    protected readonly default_value = { id: -1, trigger: "", description: "" };
+}
+
+export class ClassicDBSpellParser extends SpellParser {
+    protected readonly pattern = /<span(?: .*?=".*?")?>(.*?): <a href="\?spell=(\d+)"(?: .*?=".*?")?>(.*?)<\/a><\/span>/g;
 
     protected postformat(parse_result: string[]): PreResolvedSpellData {
-        const trigger = parse_result[1].replace(/:/g, "").trim();
+        const trigger = parse_result[1];
+        const id = Number.parseInt(parse_result[2]);
+        const description = parse_result[3];
+
+        return { id, trigger, description };
+    }
+}
+
+export class TBCDBSpellParser extends SpellParser {
+    protected readonly pattern = /<span(?: .*?=".*?")?>(.*?): (?:(?:<a href="\?spell=(\d+)"(?: .*?=".*?")?>(.*?)<\/a>)|(.*?))<\/span>/g;
+
+    protected postformat(parse_result: string[]): PreResolvedSpellData {
+        if (parse_result[2] === undefined) {
+            return this.postformat_simple_spell(parse_result);
+        }
+        return this.postformat_complex_spell(parse_result);
+    }
+
+    private postformat_simple_spell(parse_result: string[]): PreResolvedSpellData {
+        const trigger = parse_result[1];
+        const description = parse_result[4];
+
+        return { id: -1, trigger, description };
+    }
+
+    private postformat_complex_spell(parse_result: string[]): PreResolvedSpellData {
+        const trigger = parse_result[1];
         const id = Number.parseInt(parse_result[2]);
         const description = parse_result[3];
 
--- test/parsers/spell.test.ts
+++ test/parsers/spell.test.ts
@@ -1,44 +1,35 @@
 /// <reference types="jest" />
-import { SpellParser } from "../../src/parsers/spell";
+import { ClassicDBSpellParser, TBCDBSpellParser } from "../../src/parsers/spell";
 import { item_page_sources } from "../resources";
 
 describe("Spell parser", () => {
     describe("ClassicDB", () => {
         it("parses spell from classicdb.ch item page source", async () => {
-            const parser = new SpellParser(item_page_sources.deaths_sting.classicdb);
-            const result = parser.parse();
+            let parser = new ClassicDBSpellParser(item_page_sources.deaths_sting.classicdb);
+            let result = parser.parse();
             expect(result).toStrictEqual([
                 { id: 15808, trigger: "Equip", description: "+38 Attack Power." },
                 { id: 7574, trigger: "Equip", description: "Increased Daggers +3." },
             ]);
 
-            //parser = new ArmorValueParser(item_page_sources.the_hungering_cold.classicdb);
-            //result = parser.parse();
-            //expect(result).toBe(140);
+            parser = new ClassicDBSpellParser(item_page_sources.barrel.classicdb);
+            result = parser.parse();
+            expect(result).toStrictEqual([]);
         });
+    });
 
-        //it("fails to parse spell from item page source with no spells", async () => {
-        //const parser = new ArmorValueParser(item_page_sources.thunderfury.classicdb);
-        //const result = parser.parse();
-        //expect(result).toBe(-1);
-        //});
-        //});
+    describe("TBCDB", () => {
+        it("parses spell from tbcdb.com item page source", async () => {
+            let parser = new TBCDBSpellParser(item_page_sources.deaths_sting.tbcdb);
+            let result = parser.parse();
+            expect(result).toStrictEqual([
+                { id: -1, trigger: "Equip", description: "Increases your expertise rating by 7." },
+                { id: 15808, trigger: "Equip", description: "Increases attack power by 38." },
+            ]);
 
-        //describe("TBCDB", () => {
-        //it("parses spell from tbcdb.com item page source", async () => {
-        //let parser = new ArmorValueParser(item_page_sources.arcanist_belt.tbcdb);
-        //let result = parser.parse();
-        //expect(result).toBe(65);
-
-        //parser = new ArmorValueParser(item_page_sources.the_hungering_cold.tbcdb);
-        //result = parser.parse();
-        //expect(result).toBe(140);
-        //});
-
-        //it("fails to parse spell from item page source with no spells", async () => {
-        //const parser = new ArmorValueParser(item_page_sources.thunderfury.tbcdb);
-        //const result = parser.parse();
-        //expect(result).toBe(-1);
-        //});
+            parser = new TBCDBSpellParser(item_page_sources.barrel.tbcdb);
+            result = parser.parse();
+            expect(result).toStrictEqual([]);
+        });
     });
 });
